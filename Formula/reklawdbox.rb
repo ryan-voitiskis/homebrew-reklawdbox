cd3f11ce4665f25b255edcd106450df38f49b735
ryan voitiskis <ryanvoitiskis@protonmail.com>
2026-03-05 22:51:10 +1100

    feat: update SHA256 for v0.1.0 release

--- Formula/reklawdbox.rb
+++ Formula/reklawdbox.rb
@@ -7,7 +7,7 @@ class Reklawdbox < Formula
   on_macos do
     on_arm do
       url "https://github.com/ryan-voitiskis/reklawdbox/releases/download/v#{version}/reklawdbox-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "3f9108c3292df39e7253ec7eea07b69cc8eec1f333c4dfb343108aa389c702a2"
     end
   end
 
